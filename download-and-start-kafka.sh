f0a225fba0a45ee4fbd5e67643ceba1830c37e24
Maksim Kostromin <daggerok@gmail.com>
2019-02-19 01:37:47 +0200

    Update download-and-start-kafka.sh

--- download-and-start-kafka.sh
+++ download-and-start-kafka.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
-KAFKA_DIR="kafka_2.12-2.1.0"
-if ! [[ -d "/tmp/${KAFKA_DIR}" ]]; then
-  wget -qO- https://www-eu.apache.org/dist/kafka/2.1.0/kafka_2.12-2.1.0.tgz | tar xvz
-  mv -f ${KAFKA_DIR} /tmp/
+export SCALA_VERSION="2.12"
+export KAFKA_VERSION="2.1.0"
+export KAFKA_NAME="kafka_${SCALA_VERSION}-${KAFKA_VERSION}"
+export KAFKA_HOME="/tmp/${KAFKA_NAME}"
+if ! [[ -d "${KAFKA_HOME}" ]]; then
+  wget -qO- https://www-eu.apache.org/dist/kafka/${KAFKA_VERSION}/${KAFKA_NAME}.tgz | tar xvz
+  mv -f ${KAFKA_NAME} /tmp/
 fi
-
-/tmp/${KAFKA_DIR}/bin/zookeeper-server-start.sh /tmp/${KAFKA_DIR}/config/zookeeper.properties &
+${KAFKA_HOME}/bin/zookeeper-server-start.sh ${KAFKA_HOME}/config/zookeeper.properties &
 sleep 3s
-/tmp/${KAFKA_DIR}/bin/kafka-server-start.sh /tmp/${KAFKA_DIR}/config/server.properties &
-
-#export KAFKA_DIR="kafka_2.12-2.1.0" ; /tmp/${KAFKA_DIR}/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic my-topic --from-beginning
+${KAFKA_HOME}/bin/kafka-server-start.sh ${KAFKA_HOME}/config/server.properties &
+#export KAFKA_HOME="/tmp/kafka_2.12-2.1.0" ; ${KAFKA_HOME}/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic my-topic --from-beginning
 #  sleep 3s
-#  /tmp/${KAFKA_DIR}/bin/kafka-topics.sh --create --zookeeper 127.0.0.1:2181 --replication-factor 1 --partitions 1 --topic test
+#  ${KAFKA_HOME}/bin/kafka-topics.sh --create --zookeeper 127.0.0.1:2181 --replication-factor 1 --partitions 1 --topic test
